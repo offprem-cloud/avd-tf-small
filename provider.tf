ce801bf3ef364270d41558629e1e79317e621e84
offprem-cloud <75175247+offprem-cloud@users.noreply.github.com>
2024-04-25 09:58:32 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,4 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
-
-# Get AzureRM Terraforn Provider
 provider "azurerm" {
-  # version = "2.31.1" #Required for AVD
-  features {}
+  features {
+  }
 }
